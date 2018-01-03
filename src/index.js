4920351acd56de605fbbe7bb664d6d1a5f63ea06
Vincent Driessen <me@nvie.com>
2018-01-03 23:24:48 +0100

    Export types, too

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @flow
 
 import annotate from './annotate';
+import type { Annotation } from './ast';
 import serialize from './serialize';
 
+export type { Annotation };
 export { annotate, serialize };
