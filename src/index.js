bf71c51cc25cf7957c0d1e3991992cdaee6489e0
Vincent Driessen <me@nvie.com>
2018-02-01 09:03:19 +0100

    Export `summarize()` at the top-level

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import annotate, { annotateField, annotateFields, annotatePairs } from './annota
 import { isAnnotation } from './ast';
 import type { Annotation } from './ast';
 import serialize from './serialize';
+import summarize from './summarize';
 import { indent } from './utils';
 
 export type { Annotation };
-export { annotate, annotateField, annotateFields, annotatePairs, indent, isAnnotation, serialize };
+export { annotate, annotateField, annotateFields, annotatePairs, indent, isAnnotation, serialize, summarize };
