2c437cab2fe939679f99806ee3a09288946c51e9
Vincent Driessen <me@nvie.com>
2019-10-23 20:15:03 +0200

    Fix TypeScript lints

--- src/types/ast.d.ts
+++ src/types/ast.d.ts
@@ -1,23 +1,26 @@
-export type Maybe<T> = T | void;
-export type ScalarAnnotation = {
-    type: 'ScalarAnnotation',
-    value: unknown,
-    hasAnnotation: boolean,
-    annotation: Maybe<string>,
-};
-export type AnnPair = { key: string, value: Annotation };
-export type ObjectAnnotation = {
-    type: 'ObjectAnnotation',
-    pairs: Array<AnnPair>,
-    hasAnnotation: boolean,
-    annotation: Maybe<string>,
-};
-export type ArrayAnnotation = {
-    type: 'ArrayAnnotation',
-    items: Array<Annotation>,
-    hasAnnotation: boolean,
-    annotation: Maybe<string>,
-};
+export type Maybe<T> = T | undefined;
+export interface ScalarAnnotation {
+    type: 'ScalarAnnotation';
+    value: unknown;
+    hasAnnotation: boolean;
+    annotation: Maybe<string>;
+}
+export interface AnnPair {
+    key: string;
+    value: Annotation;
+}
+export interface ObjectAnnotation {
+    type: 'ObjectAnnotation';
+    pairs: AnnPair[];
+    hasAnnotation: boolean;
+    annotation: Maybe<string>;
+}
+export interface ArrayAnnotation {
+    type: 'ArrayAnnotation';
+    items: Annotation[];
+    hasAnnotation: boolean;
+    annotation: Maybe<string>;
+}
 export type Annotation = ObjectAnnotation | ArrayAnnotation | ScalarAnnotation;
 export function asAnnotation(thing: unknown): Annotation | undefined;
 export function isAnnotation(thing: unknown): boolean;
--- src/types/summarize.d.ts
+++ src/types/summarize.d.ts
@@ -1,5 +1,5 @@
 import { Annotation } from './ast';
 
-type Keypath = Array<number | string>;
+export type Keypath = Array<number | string>;
 
-export default function summarize(ann: Annotation, keypath?: Keypath): Array<string>;
+export default function summarize(ann: Annotation, keypath?: Keypath): string[];
