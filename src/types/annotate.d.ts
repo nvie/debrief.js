663aa638c9e47d7b98483c2ceb44f5f1564aa510
Vincent Driessen <me@nvie.com>
2020-05-25 13:42:36 +0200

    Fix TypeScript types

--- src/types/annotate.d.ts
+++ src/types/annotate.d.ts
@@ -2,6 +2,7 @@ import { asAnnotation, Annotation, ObjectAnnotation } from './ast';
 
 export function annotateFields(
     object: { [field: string]: unknown },
-    fields: Array<[string, string | Annotation]>
+    fields: Array<[string, string | Annotation]>,
+    _seen?: WeakSet<object>
 ): ObjectAnnotation;
-export default function annotate(value: unknown, annotation?: string): Annotation;
+export default function annotate(value: unknown, annotation?: string, _seen?: WeakSet<object>): Annotation;
